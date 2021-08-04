e4140ce258bf0064f5afbc1f9effe4b552abc891
Allen Lin <allenlin1992@hotmail.com>
2021-08-04 17:07:42 -0400

    LFG

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
+import tokenVaultAbi from "./abis/token-vault-abi.json";
+import { useEffect } from 'react';
+
+const HOODIE_ADDRESS = "0xdffa3a7f5b40789c7a437dbe7b31b47f9b08fe75";
 
 function App() {
+  const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/6e758ef5d39a4fdeba50de7d10d08448");
+  const hoodieContract = new ethers.Contract(HOODIE_ADDRESS, tokenVaultAbi, provider);
+
+  useEffect(() => {
+    async function demo() {
+      console.log("Name: ", await hoodieContract.name());
+      console.log("Reserve Price: ", ethers.utils.formatEther(await hoodieContract.reservePrice()));
+      console.log("Total Supply: ", ethers.utils.formatEther(await hoodieContract.totalSupply()));
+    }
+    demo();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Punk Parts</h1>
     </div>
   );
 }
