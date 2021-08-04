fbb0436d525df9dde799d81208ac02317632fe58
Allen Lin <allenlin1992@hotmail.com>
2021-08-04 18:00:38 -0400

    continue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import { Container, Col, Row, Table } from 'react-bootstrap';
 import { ethers } from "ethers";
 import tokenVaultAbi from "./abis/token-vault-abi.json";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import punk7171 from './images/punk7171.png'
 
 const HOODIE_ADDRESS = "0xdffa3a7f5b40789c7a437dbe7b31b47f9b08fe75";
 
@@ -9,19 +11,27 @@ function App() {
   const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/6e758ef5d39a4fdeba50de7d10d08448");
   const hoodieContract = new ethers.Contract(HOODIE_ADDRESS, tokenVaultAbi, provider);
 
+  const [name, setName] = useState();
+  const [reservePrice, setReservePrice] = useState();
+  const [totalSupply, setTotalSupply] = useState();
+
+  // Initially retrieve HOODIE's contract data
   useEffect(() => {
     async function demo() {
-      console.log("Name: ", await hoodieContract.name());
-      console.log("Reserve Price: ", ethers.utils.formatEther(await hoodieContract.reservePrice()));
-      console.log("Total Supply: ", ethers.utils.formatEther(await hoodieContract.totalSupply()));
+      setName(await hoodieContract.name());
+      setReservePrice(await hoodieContract.reservePrice());
+      setTotalSupply(await hoodieContract.totalSupply());
     }
     demo();
   }, []);
 
   return (
-    <div>
-      <h1>Punk Parts</h1>
-    </div>
+    <Container>
+      <h1 className="text-center">{name}</h1>
+      <div className="text-center">
+        <img src={punk7171} width="250px"></img>
+      </div>
+    </Container>
   );
 }
 
