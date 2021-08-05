076181cca9f92fb66480675a3c5653f31b99087b
Allen Lin <allenlin1992@hotmail.com>
2021-08-05 15:42:41 -0400

    continue

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import punk7171 from './images/punk7171.png'
 
 const HOODIE_ADDRESS = "0xdffa3a7f5b40789c7a437dbe7b31b47f9b08fe75";
 const POOL_ADDRESS = "0xf1a8f0d86659c67780e3396bd6aee05af3566c6a";
-const APIURL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+const UNISWAP_V3_APIURL = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
 
 const poolQuery = `
   {
     pool (id: "${POOL_ADDRESS}") {
+      token0 {
+        symbol
+      },
+      token1 {
+        symbol
+      },
       token0Price,
       token1Price,
       totalValueLockedToken0,
@@ -21,6 +27,13 @@ const poolQuery = `
         timestamp,
         amount0,
         amount1
+      },
+      poolHourData {
+        periodStartUnix
+        open,
+        high,
+        low,
+        close
       }
     }
   }
@@ -30,21 +43,45 @@ function App() {
   const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/6e758ef5d39a4fdeba50de7d10d08448");
   const hoodieContract = new ethers.Contract(HOODIE_ADDRESS, tokenVaultAbi, provider);
   const client = new ApolloClient({
-    uri: APIURL,
+    uri: UNISWAP_V3_APIURL,
     cache: new InMemoryCache()
   });
 
+  // from HOODIE contract (fractional's tokenVault) using ethers
   const [name, setName] = useState();
+  const [symbol, setSymbol] = useState();
   const [reservePrice, setReservePrice] = useState();
   const [totalSupply, setTotalSupply] = useState();
 
-  // Initially retrieve HOODIE's contract data
+  // from UNISWAP V3 graph
+  const [token0, setToken0] = useState();
+  const [token1, setToken1] = useState();
+  const [token0Price, setToken0Price] = useState();
+  const [token1Price, setToken1Price] = useState();
+  const [liquidityToken0, setLiquidityToken0] = useState();
+  const [liquidityToken1, setLiquidityToken1] = useState();
+  const [swaps, setSwaps] = useState([]);
+
+
+  // Initially retrieve data
   useEffect(() => {
     async function demo() {
+      // Ethers contract data
       setName(await hoodieContract.name());
+      setSymbol(await hoodieContract.symbol());
       setReservePrice(ethers.utils.formatEther(await hoodieContract.reservePrice()));
       setTotalSupply(ethers.utils.formatEther(await hoodieContract.totalSupply()));
-      console.log(await client.query({query: gql(poolQuery)}));
+      
+      // Graph data
+      let graphData = (await client.query({query: gql(poolQuery)})).data.pool;
+      console.log(graphData);
+      setToken0(graphData.token0.symbol);
+      setToken1(graphData.token1.symbol);
+      setToken0Price(graphData.token0Price);
+      setToken1Price(graphData.token1Price);
+      setLiquidityToken0(graphData.totalValueLockedToken0);
+      setLiquidityToken1(graphData.totalValueLockedToken1);
+      setSwaps(graphData.swaps);
     }
     demo();
   }, []);
@@ -59,9 +96,20 @@ function App() {
       </Row>
       <Row>
         <Col>
+          <div><strong>Token:&nbsp;</strong>${symbol}</div>
           <div><strong>Reserve Price:&nbsp;</strong>{reservePrice}</div>
           <div><strong>Total Supply:&nbsp;</strong>{totalSupply}</div>
+          <div><strong>{token1} Price:&nbsp;</strong>{token0Price} {token0}</div>
+          <div><strong>{token0} Price:&nbsp;</strong>{token1Price} {token1}</div>
+          <div><strong>{token0} Liquidity:&nbsp;</strong>{liquidityToken0} {token0}</div>
+          <div><strong>{token1} Liquidity:&nbsp;</strong>{liquidityToken1} {token1}</div>
+          <div><strong>Implied Valuation:&nbsp;</strong>{parseFloat(totalSupply)*parseFloat(token0Price)} {token0}</div>
         </Col>
+      </Row>
+      <Row>
+        <Table>
+          <thead></thead>
+        </Table>
       </Row>
     </Container>
   );
